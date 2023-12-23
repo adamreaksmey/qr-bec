96af4afaedb5d0cafdad090a1bb3f9784b12a444
adamreaksmey <adaminiature@gmail.com>
2023-12-23 17:20:52 +0700

    installed bulma

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,26 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome!</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/church.svg') }}">
+    <title>Hello!</title>
     @vite('resources/css/app.css')
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="flex flex-col justify-center pt-20 gap-10 w-full">
+        <div class="text-3xl font-bold flex justify-center">
+            Are you arriving or leaving?
+        </div>
+        <div class="flex justify-center gap-2">
+            <button class="button is-primary">Arriving</button>
+            <button class="button is-danger">Leaving</button>
+        </div>
+        <div class="flex justify-center items-end">
+            <button class="button is-warning">ខ្មែរ | English</button>
+        </div>
+    </div>
 </body>
 
 
