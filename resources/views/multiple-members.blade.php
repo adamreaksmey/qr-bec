5f964ec4f2e7130d590369e77cce6c2da0abbd2d
adamreaksmey <adaminiature@gmail.com>
2023-12-24 14:23:55 +0700

    updated page

--- resources/views/multiple-members.blade.php
+++ resources/views/multiple-members.blade.php
@@ -4,5 +4,27 @@
 <x-head />
 
 <body>
-    <div>hello world</div>
+    <div class="h-screen flex justify-center items-center">
+        <div class="flex flex-col">
+            <div class="text-3xl pb-10">Who in your group is leaving?</div>
+            <div class="text-2xl">
+                <label class="checkbox">
+                    <input type="checkbox" class="form-checkbox h-4 w-4">
+                    <span class="ml-2">Adam</span>
+                </label>
+            </div>
+            <div class="text-2xl">
+                <label class="checkbox">
+                    <input type="checkbox" class="form-checkbox h-4 w-4">
+                    <span class="ml-2">Zac</span>
+                </label>
+            </div>
+            <div class="text-2xl">
+                <label class="checkbox">
+                    <input type="checkbox" class="form-checkbox h-4 w-4">
+                    <span class="ml-2">Mey Mey</span>
+                </label>
+            </div>
+        </div>
+    </div>
 </body>
