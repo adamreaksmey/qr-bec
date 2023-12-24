635af8231bb134256585f157a5d5ff0a476c6901
adamreaksmey <adaminiature@gmail.com>
2023-12-24 14:25:03 +0700

    added jquery

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,6 +3,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/church.svg') }}">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxchimp/1.3.0/jquery.ajaxchimp.min.js"></script>
     <title>Hello!</title>
     @vite('resources/css/app.css')
 </head>
