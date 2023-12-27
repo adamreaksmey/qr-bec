912eca719bcce5390ad64b7249c670040d77aa75
adamreaksmey <adaminiature@gmail.com>
2023-12-27 22:43:54 +0700

    temporary auth

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,3 +6,24 @@
 <body>
     <div>Hello from dashboard!</div>
 </body>
+<script>
+    function redirectDashBoard(token) {
+        $.ajax({
+            url: '/api/bec/get-registered-user',
+            type: 'GET',
+            headers: {
+                "Authorization": "Bearer " + token,
+            },
+            success: function(response) {
+                if (response.data.authUser) {
+                    return
+                }
+                window.location.href = '/login';
+                return
+            }
+        })
+    }
+
+    const token = localStorage.getItem('token')
+    redirectDashBoard(token)
+</script>
